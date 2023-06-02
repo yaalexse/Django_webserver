eedac57f60580e2987f4c870181384b4ef614d3a
yaalexse <3juigb3@gmail.com>
2023-06-02 06:54:43 +0200

    Ressources added for depoyement

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -17,7 +17,7 @@ DEBUG = config('DJANGO_DEBUG')
 
 #ALLOWED_HOSTS = ['forthallgym.herokuapp.com','127.0.0.1']
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['forthallgym.herokuapp.com','127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
